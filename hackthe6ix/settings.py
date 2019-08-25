4956dd71105a4152208b0facd8e073807251ae97
bhatts39 <sunnynbhatt@gmail.com>
2019-08-25 11:46:12 -0400

    .

--- hackthe6ix/settings.py
+++ hackthe6ix/settings.py
@@ -130,5 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = '/donor/static'
 STATIC_URL = '/static/'
-STATIC_ROOT = '/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
