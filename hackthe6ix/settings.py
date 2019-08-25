e5649a7a48300915c926e14a10727b4fef67b211
bhatts39 <sunnynbhatt@gmail.com>
2019-08-25 04:22:54 -0400

    MOE FEIHGKF CSRF

--- hackthe6ix/settings.py
+++ hackthe6ix/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'donor.disable.DisableCSRF'
 ]
 
 ROOT_URLCONF = 'hackthe6ix.urls'
